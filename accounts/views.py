5c2ef0f5616f8886ace07d3c9d36b58d4c72c411
Darveloper <darveloper@gmail.com>
2020-06-21 13:28:14 -0400

    login required

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-
+from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm
 
+@login_required
 class SignUpView(CreateView):
     form_class = RegisterForm
     success_url = reverse_lazy('login')
